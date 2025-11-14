2fe283001a377052dfe8f3d825e71560440ed917
Lopezzz6 <103112629+Lopezzz6@users.noreply.github.com>
2025-11-14 17:25:01 +0000

    Adicionar arquivos de projeto.

---
+++ ProjetoPOO/condominio.cs
@@ -0,0 +1,80 @@
+﻿#region author info
+// ============================================================
+// Author: Rodrigo Lopes Ferreira
+// Date:    {11/09/2025}
+// Project: {Gestão de Condomínios}
+// File:    {condominio.cs}
+//
+// Description:
+//   {Projeto sobre gestão de condomínios em C#}
+//
+// ============================================================
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestCondominios
+{
+    ///Classes
+    class Condominio
+	{
+        #region Attributes
+
+
+
+        #endregion
+
+
+        #region Properties
+
+
+
+        #endregion
+
+
+        #region Methods
+
+
+
+        #endregion
+
+
+        #region Constructor
+
+
+
+        #endregion
+
+
+        #region Operators
+
+
+
+        #endregion
+
+
+        #region Protected
+
+
+
+        #endregion
+
+
+        #region Overrides
+
+
+
+        #endregion
+
+
+        #region Destructor
+
+
+
+        #endregion
+    }
+}
---
+++ ProjetoPOO/program.cs
@@ -0,0 +1,25 @@
+﻿#region author info
+// ============================================================
+// Author: Rodrigo Lopes Ferreira
+// Date:    {11/09/2025}
+// Project: {Gestão de Condomínios}
+// File:    {program.cs}
+//
+// Description:
+//   {Projeto sobre gestão de condomínios em C#}
+//
+// ============================================================
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    class Program
+    {
+    }
+}
